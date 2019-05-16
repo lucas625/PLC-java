cd99c6afd303cd5c4eda6000a11d8d06e35892d3
lucas625 <lagc@cin.ufpe.br>
2019-05-16 09:45:43 -0300

    aula 2 done

--- codigos estudo/Aula02.java
+++ codigos estudo/Aula02.java
@@ -1,22 +1,58 @@
 public class Aula02 {
     public static void main(String args[]) {
+
         System.out.println("hello world");
+        Counter c = new Counter();
+        Thread t1 = new Thread(c);
+        Thread t2 = new Thread(c);
+        t1.start();
+        t2.start();
     }
 
 }
-public class Counter {
+class Counter implements Runnable {
     private long valor;
+    private boolean primeiro;
 
     Counter() {
         this.valor = 0;
+        this.primeiro = true;
     }
 
-    public long getAndIncrement() {
+    public long getValor() {
+        return this.valor;
+    }
+
+    public void getAndIncrement() {
         long temp;
         synchronized(this){
             temp = this.valor;
             this.valor = temp + 1;
         }
-        return temp;
+    }
+
+    public void makerun(int indice) {
+        synchronized (this) {
+            getAndIncrement();
+            System.out.println("t" + indice + " valor: " + getValor());
+            try {
+                Thread.sleep(100);
+            } catch(InterruptedException ie) {
+                System.out.println("não deu para parar");
+            }
+        }
+    }
+
+    public void run() {
+        int indice = 0;
+        if (this.primeiro) {
+            this.primeiro = false;
+            indice = 1;
+        }else {
+            indice = 2;
+        }
+        for(int i=0;i<50;i++) {
+            makerun(indice);
+        }
     }
 }
